0dffc8bdca3b10736de854a5e9b44b8821c43f36
Gavin He <hscgavin@gmail.com>
2017-06-03 11:18:42 +1000

    Create ListModel to store data
    ListModel reveal a function {removeItem} which can be called by a controller

--- app/script.js
+++ app/script.js
@@ -6,6 +6,35 @@
   'use strict';
 
 
+  /**
+   * Creates a Model instance
+   *
+   * @constructor
+   * @param {object} items from API ({items_last_week": []})
+   */
+
+  function ListModel(items) {
+    this.items = items || [];
+  }
+
+  ListModel.prototype = {
+
+  /**
+	 * Removes an item
+	 *
+	 * @param {number} id of the item to remove
+	 * @param {function} callback run after removal is complete.
+	 */
+    removeItem: function (id, callback) {
+      for (var i = 0; i < this.items.length; i++) {
+        if (this.items[i].id == id) {
+          this.items.splice(i, 1);
+          break;
+        }
+      }
+      callback.call(this, this.items);
+    }
+  }
 
 
 })(window);
